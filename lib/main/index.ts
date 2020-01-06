5910d6b3eee87bcdb5f36952e83684a6d89ef620
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-01-06 13:25:26 +0100

    Add Json type

--- lib/main/index.ts
+++ lib/main/index.ts
@@ -2,3 +2,5 @@ export * from "./Async"
 export * from "./MapTransform"
 export * from "./Lazy"
 export * from "./Observable"
+
+export type Json = string | number | boolean | null | { [property: string]: Json } | Json[]
