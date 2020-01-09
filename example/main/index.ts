ac6f3df84fff2d01d621a35e0376785d8d9e75e9
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-01-09 16:42:50 +0100

    Fix CI failure

--- example/main/index.ts
+++ example/main/index.ts
@@ -1,8 +1,10 @@
 import { ConsoleInput } from "./ConsoleInput"
 import { Application } from "./Application"
 
-const input = new ConsoleInput()
-console.log("To start running the server, press 's'.")
-input.once("escape", () => process.exit(0))
-input.once("ctrl+c", () => process.exit(0))
-input.once("s", () => new Application(input).start().catch(console.error))
+if("setRawMode" in process.stdin) {
+    const input = new ConsoleInput()
+    console.log("To start running the server, press 's'.")
+    input.once("escape", () => process.exit(0))
+    input.once("ctrl+c", () => process.exit(0))
+    input.once("s", () => new Application(input).start().catch(console.error))
+}
