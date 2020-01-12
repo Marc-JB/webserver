830faf5ab0fe5996c83737b5b7b1962945b71edc
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-01-12 19:47:52 +0100

    Fix test, remove WebServer dependency

--- src/test/Endpoint.ts
+++ src/test/Endpoint.ts
@@ -1,13 +1,12 @@
 import { suite, test, expect } from "../../lib/test/index"
-import { Endpoint, WebServer } from "../main/index"
+import { Endpoint } from "../main/index"
 
 @suite
 export class EndpointTests {
     @test
     async createEndpointAtPathShouldAttachEndpointToRightPath() {
         // Arrange
-        const server = await new WebServer.Builder().enableDevelopmentMessages().build()
-        const endpoint1 = new Endpoint("/resource/", server)
+        const endpoint1 = new Endpoint("/resource/")
 
         // Act
         const endpoint2 = endpoint1.createEndpointAtPath("/subresource/")
